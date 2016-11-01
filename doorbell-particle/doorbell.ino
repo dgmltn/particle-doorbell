24ccd3b103ba19a9d6221030cebfd0b67effab89
Doug Melton <dmelton@gopro.com>
2016-11-01 08:46:30 -0700

    Changed ringtone for halloween

--- doorbell-particle/doorbell.ino
+++ doorbell-particle/doorbell.ino
@@ -22,10 +22,10 @@
 // http://ez4mobile.com/nokiatone/rtttf.htm
 
 // A fun program for the Spark Core that works without the use of the tone() function
-// Originally written by Brett Hagman (http://roguerobotics.com), 
+// Originally written by Brett Hagman (http://roguerobotics.com),
 // Reworked by Technobly (http://technobly.com) for the Spark Core.
 //
-// This plays RTTTL (RingTone Text Transfer Language) songs 
+// This plays RTTTL (RingTone Text Transfer Language) songs
 // by bit-banging a selected digital output.
 //
 // You can get more RTTTL songs from
@@ -49,12 +49,13 @@ char* song = (char*)
 //"20thCenFox:d=16,o=5,b=140:b,8p,b,b,2b,p,c6,32p,b,32p,c6,32p,b,32p,c6,32p,b,8p,b,b,b,32p,b,32p,b,32p,b,32p,b,32p,b,32p,b,32p,g#,32p,a,32p,b,8p,b,b,2b,4p,8e,8g#,8b,1c#6,8f#,8a,8c#6,1e6,8a,8c#6,8e6,1e6,8b,8g#,8a,2b";
 //"2.34kHzBeeps:d=4,o=7,b=240:d,p,d,p,d,p,d,p";
 //"A-Team:d=8,o=5,b=125:4d#6,a#,2d#6,16p,g#,4a#,4d#.,p,16g,16a#,d#6,a#,f6,2d#6,16p,c#.6,16c6,16a#,g#.,2a#";
+"Addams Family:d=8,o=5,b=160:c,4f,a,4f,c,4b4,2g,f,4e,g,4e,g4,4c,2f,c,4f,a,4f,c,4b4,2g,f,4e,c,4d,e,1f,c,d,e,f,1p,d,e,f#,g,1p,d,e,f#,g,4p,d,e,f#,g,4p,c,d,e,f";
 //"AlarmDoorBeepBeep:d=4,o=7,b=125:16e,16p,16e";
 //"AlarmDoorBeepBoop:d=4,o=7,b=125:16e,16p,16c";
 //"Bond:d=4,o=5,b=80:32p,16c#6,32d#6,32d#6,16d#6,8d#6,16c#6,16c#6,16c#6,16c#6,32e6,32e6,16e6,8e6,16d#6,16d#6,16d#6,16c#6,32d#6,32d#6,16d#6,8d#6,16c#6,16c#6,16c#6,16c#6,32e6,32e6,16e6,8e6,16d#6,16d6,16c#6,16c#7,c.7,16g#6,16f#6,g#.6";
 //"Barbie girl:d=4,o=5,b=125:8g#,8e,8g#,8c#6,a,p,8f#,8d#,8f#,8b,g#,8f#,8e,p,8e,8c#,f#,c#,p,8f#,8e,g#,f#,8g#,8e,8g#,8c#6,a,p,8f#,8d#,8f#,8b,g#,8f#,8e,p,8e,8c#,f#,c#,p,8f#,8e,g#,f#,8g#,8e,8g#,8c#6,a,p,8f#,8d#,8f#,8b,g#,8f#,8e,p,8e,8c#,f#,c#,p,8f#,8e,g#,f#";
 // http://www.virtuallysmoothpiano.com/uploads/5/4/9/1/5491021/heads_ringing_piano.pdf
-"DrDre_Keep_Their_Heads_Ringing:d=4,o=5,b=100:4a4,4e,2d,8a4,16a4,8a4,8e,8e,2d";
+//"DrDre_Keep_Their_Heads_Ringing:d=4,o=5,b=100:4a4,4e,2d,8a4,16a4,8a4,8e,8e,2d";
 //"Entertainer:d=4,o=5,b=140:8d,8d#,8e,c6,8e,c6,8e,2c.6,8c6,8d6,8d#6,8e6,8c6,8d6,e6,8b,d6,2c6,p,8d,8d#,8e,c6,8e,c6,8e,2c.6,8p,8a,8g,8f#,8a,8c6,e6,8d6,8c6,8a,2d6";
 //"Flinstones:d=4,o=5,b=40:32p,16f6,16a#,16a#6,32g6,16f6,16a#.,16f6,32d#6,32d6,32d6,32d#6,32f6,16a#,16c6,d6,16f6,16a#.,16a#6,32g6,16f6,16a#.,32f6,32f6,32d#6,32d6,32d6,32d#6,32f6,16a#,16c6,a#,16a6,16d.6,16a#6,32a6,32a6,32g6,32f#6,32a6,8g6,16g6,16c.6,32a6,32a6,32g6,32g6,32f6,32e6,32g6,8f6,16f6,16a#.,16a#6,32g6,16f6,16a#.,16f6,32d#6,32d6,32d6,32d#6,32f6,16a#,16c.6,32d6,32d#6,32f6,16a#,16c.6,32d6,32d#6,32f6,16a#6,16c7,8a#.6";
 //"Gadget:d=16,o=5,b=50:32d#,32f,32f#,32g#,a#,f#,a,f,g#,f#,32d#,32f,32f#,32g#,a#,d#6,4d6,32d#,32f,32f#,32g#,a#,f#,a,f,g#,f#,8d#";
@@ -74,7 +75,7 @@ char* song = (char*)
 //"SMBtheme:d=4,o=5,b=100:16e6,16e6,32p,8e6,16c6,8e6,8g6,8p,8g,8p,8c6,16p,8g,16p,8e,16p,8a,8b,16a#,8a,16g.,16e6,16g6,8a6,16f6,8g6,8e6,16c6,16d6,8b,16p,8c6,16p,8g,16p,8e,16p,8a,8b,16a#,8a,16g.,16e6,16g6,8a6,16f6,8g6,8e6,16c6,16d6,8b,8p,16g6,16f#6,16f6,16d#6,16p,16e6,16p,16g#,16a,16c6,16p,16a,16c6,16d6,8p,16g6,16f#6,16f6,16d#6,16p,16e6,16p,16c7,16p,16c7,16c7,p,16g6,16f#6,16f6,16d#6,16p,16e6,16p,16g#,16a,16c6,16p,16a,16c6,16d6,8p,16d#6,8p,16d6,8p,16c6";
 //"SMBunderground:d=16,o=6,b=100:c,c5,a5,a,a#5,a#,2p,8p,c,c5,a5,a,a#5,a#,2p,8p,f5,f,d5,d,d#5,d#,2p,8p,f5,f,d5,d,d#5,d#,2p,32d#,d,32c#,c,p,d#,p,d,p,g#5,p,g5,p,c#,p,32c,f#,32f,32e,a#,32a,g#,32p,d#,b5,32p,a#5,32p,a5,g#5";
 //"SMBwater:d=8,o=6,b=225:4d5,4e5,4f#5,4g5,4a5,4a#5,b5,b5,b5,p,b5,p,2b5,p,g5,2e.,2d#.,2e.,p,g5,a5,b5,c,d,2e.,2d#,4f,2e.,2p,p,g5,2d.,2c#.,2d.,p,g5,a5,b5,c,c#,2d.,2g5,4f,2e.,2p,p,g5,2g.,2g.,2g.,4g,4a,p,g,2f.,2f.,2f.,4f,4g,p,f,2e.,4a5,4b5,4f,e,e,4e.,b5,2c.";
-//"SMBdeath:d=4,o=5,b=90:32c6,32c6,32c6,8p,16b,16f6,16p,16f6,16f.6,16e.6,16d6,16c6,16p,16e,16p,16c"; 
+//"SMBdeath:d=4,o=5,b=90:32c6,32c6,32c6,8p,16b,16f6,16p,16f6,16f.6,16e.6,16d6,16c6,16p,16e,16p,16c";
 //"Smurfs:d=32,o=5,b=200:4c#6,16p,4f#6,p,16c#6,p,8d#6,p,8b,p,4g#,16p,4c#6,p,16a#,p,8f#,p,8a#,p,4g#,4p,g#,p,a#,p,b,p,c6,p,4c#6,16p,4f#6,p,16c#6,p,8d#6,p,8b,p,4g#,16p,4c#6,p,16a#,p,8b,p,8f,p,4f#";
 //"StarWars:d=4,o=5,b=45:32p,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#.6,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#6";
 //"The Simpsons:d=4,o=5,b=168:c.6,e6,f#6,8a6,g.6,e6,c6,8a,8f#,8f#,8f#,2g,8p,8p,8f#,8f#,8f#,8g,a#.,8c6,8c6,8c6,c6";
@@ -89,10 +90,10 @@ char* song = (char*)
 
 #define OCTAVE_OFFSET 0
 
-// Notes defined in microseconds (Period/2) 
+// Notes defined in microseconds (Period/2)
 // from note C to B, Octaves 3 through 7
 // Convert to frequency by f = 500000 / notes[i]
-int notes[] = 
+int notes[] =
 {0,
 3817,3597,3401,3205,3030,2857,2703,2551,2404,2273,2146,2024,
 1908,1805,1701,1608,1515,1433,1351,1276,1205,1136,1073,1012,
@@ -175,7 +176,7 @@ bool next_rtttl() {
     {
       num = (num * 10) + (*p++ - '0');
     }
-    
+
     if(num) duration = wholenote / num;
     else duration = wholenote / default_dur;  // we will need to check if we are a dotted note after
 
@@ -224,7 +225,7 @@ bool next_rtttl() {
       duration += duration/2;
       p++;
     }
-  
+
     // now, get scale
     if(isdigit(*p))
     {
@@ -294,13 +295,13 @@ void tone(int freq) {
 
 void playback_handler(void) {
     int s = SINE[(int)t];
-    
+
     // The "+" side of the signal: 127 = 0, 255 = 4096
     analogWrite(DAC1, s <= 127 ? 0 : (s - 127) << VOLUME);
-    
+
     // The "-" side of the signal: 127 = 0, 0 = 4096
     analogWrite(DAC2, s >= 127 ? 0 : (127 - s) << VOLUME);
-    
+
     // Increment t
     t += step;
     while (t > SINE_LENGTH) t -= SINE_LENGTH;
@@ -374,7 +375,7 @@ void setupWebserver() {
     IPAddress myIp = WiFi.localIP();
     sprintf(myIpString, "%d.%d.%d.%d", myIp[0], myIp[1], myIp[2], myIp[3]);
     Particle.variable("ipAddress", myIpString, STRING);
-    
+
     /* run the same command if you try to load /index.html, a common
      * default page name */
     webserver.addCommand("beepbeep", &webserver_beepbeep);
@@ -404,10 +405,10 @@ void setup(void) {
 
     digitalWrite(PIN_LED, LOW);
     digitalWrite(PIN_AMP_ENABLE, LOW);
- 
+
     Particle.function("doorbell", particle_doorbell);
     Particle.function("beepbeep", particle_beepbeep);
-    
+
     setupWebserver();
  }
 
@@ -421,7 +422,7 @@ void loop(void) {
     if (playing) {
         // Play next note
         bool next = next_rtttl();
-        
+
         // Check to see if that was the last note
         if (!next) {
             digitalWrite(PIN_LED, LOW); // Turn off the onboard Blue LED.
@@ -431,16 +432,16 @@ void loop(void) {
             analogWrite(DAC2, 0);
         }
     }
-    
+
     else {
         // Read the state of the pushbutton value
         bool state = digitalRead(PIN_DOORBELL_BUTTON) == LOW;
-    
+
         // Check if the pushbutton changed value
         if (state != clicked) {
             delay(500); // Debounce
             clicked = state;
-    
+
             // They pressed the button. Start the music
             if (clicked) {
                 Particle.publish("doorbell", NULL, 60, PRIVATE);
@@ -451,4 +452,3 @@ void loop(void) {
         loopWebserver();
     }
 }
-
